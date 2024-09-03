33ea973fc8ebe3fa09981f5df375dfc2be4736e4
nahidbinalamsami <saminahidbinalam@gmail.com>
2024-09-04 04:39:39 +0600

    my javascript love

--- milstone04/Array/secend.js
+++ milstone04/Array/secend.js
@@ -1,4 +1,7 @@
-const place = ['coxs', 'sylhet', 'cumilla']
-place.push('sajek','ctg',);
-place.pop('cgt');
-console.log(place);
+// const place = ['coxs', 'sylhet', 'cumilla']
+// place.push('sajek','ctg',);
+// place.pop('cgt');
+// console.log(place);
+const fruits = ['mango', 'orange']
+console.log(fruits)
+git hub kena pataiyom
